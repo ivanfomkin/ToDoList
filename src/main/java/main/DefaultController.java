3efcd03e35ff15d7273986373ff406ffad74cc8e
ivan fomkin <ivan-fomkin@yandex.ru>
2020-03-25 22:41:38 +0300

    My Firs SpringBoot application done

---
+++ src/main/java/main/DefaultController.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public int index() {
+        return (int) (Math.random() * 1_000_000);
+    }
+}
